87215c8a375b499a762135cbf75d705ca19df050
JMMatuszewski <jmm.matuszewski.programista@gmail.com>
2024-03-21 19:24:31 +0100

    Add .mm file

---
+++ Blackjack.mm
@@ -0,0 +1,30 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1710869719503" ID="ID_284551761" MODIFIED="1710874417704" TEXT="Blackjack">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1710870513004" ID="ID_706648861" MODIFIED="1710875416465" POSITION="right" STYLE="bubble" TEXT="Bud&#x17c;et">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1710874570411" ID="ID_1105011489" MODIFIED="1710875414162" TEXT="Operacje bud&#x17c;etem"/>
+<node CREATED="1710874588773" ID="ID_344352497" MODIFIED="1710875414162" TEXT="Pilnowanie wyzerowania - koniec gry"/>
+<node CREATED="1710938800928" ID="ID_1511272885" MODIFIED="1710938805444" TEXT="Obstawianie zak&#x142;ad&#xf3;w"/>
+</node>
+<node CREATED="1710870533979" ID="ID_1532391396" MODIFIED="1710936065214" POSITION="right" STYLE="bubble" TEXT="Operacje kartami">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1710936069372" ID="ID_1144054566" MODIFIED="1710938676169" TEXT="Przygotowanie talii bez powtarzaj&#x105;cych si&#x119; kart"/>
+<node CREATED="1710870604677" ID="ID_304404487" MODIFIED="1710938690594" TEXT="Liczenie kart gracza i krupiera">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1710936096398" ID="ID_1194502727" MODIFIED="1710936104864" TEXT="Dobieranie kart z wierzchu stosu"/>
+</node>
+<node CREATED="1710874378440" ID="ID_1803138598" MODIFIED="1710931237833" POSITION="left" STYLE="bubble" TEXT="Oprawa graficzna">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1710874513779" ID="ID_1998092174" MODIFIED="1710938716955" STYLE="bubble" TEXT="Szablon karty"/>
+<node CREATED="1710938717446" ID="ID_714983515" MODIFIED="1710938724334" TEXT="Szablon zakrytej karty"/>
+<node CREATED="1710874536632" ID="ID_119121175" MODIFIED="1710938755344" STYLE="bubble" TEXT="Modyfikacja szablonu w zale&#x17c;no&#x15b;ci od rodzaju karty"/>
+<node CREATED="1710936148381" ID="ID_1440144326" MODIFIED="1710936154590" TEXT="Wypisanie kart na ekranie"/>
+</node>
+</node>
+</map>

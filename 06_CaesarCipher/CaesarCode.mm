dae4934c8398387ab919373bd0ea10ce8cff25ef
JMMatuszewski <jmm.matuszewski.programista@gmail.com>
2024-03-23 15:02:47 +0100

    First upload of CaesarCipher

---
+++ 06_CaesarCipher/CaesarCode.mm
@@ -0,0 +1,17 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1711197053264" ID="ID_387079212" MODIFIED="1711197385645" STYLE="bubble" TEXT="Caesar Code">
+<node CREATED="1711197072134" ID="ID_1211616795" MODIFIED="1711197385646" POSITION="right" STYLE="bubble" TEXT="Option to choose">
+<node CREATED="1711197150299" ID="ID_1167096060" MODIFIED="1711197385646" STYLE="bubble" TEXT="Encode"/>
+<node CREATED="1711197160274" ID="ID_82078879" MODIFIED="1711197385646" STYLE="bubble" TEXT="Decode"/>
+</node>
+<node CREATED="1711197241589" ID="ID_61103624" MODIFIED="1711197385646" POSITION="right" STYLE="bubble" TEXT="Considered symbols"/>
+<node CREATED="1711197187091" ID="ID_762484104" MODIFIED="1711197385646" POSITION="right" STYLE="bubble" TEXT="Number of jumps">
+<node CREATED="1711197218207" ID="ID_273870806" MODIFIED="1711197385646" STYLE="bubble" TEXT="0 to number of symbols"/>
+<node CREATED="1711197311813" ID="ID_577421845" MODIFIED="1711197385642" STYLE="bubble" TEXT="Circle back to beginning">
+<edge COLOR="#808080" STYLE="bezier" WIDTH="thin"/>
+</node>
+<node CREATED="1711197437901" ID="ID_1224816809" MODIFIED="1711197446161" TEXT="Ignore white space"/>
+</node>
+</node>
+</map>

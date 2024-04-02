8a8b4e900b7f3c5b7cead20d31f1998951fa8b6d
JMMatuszewski <jmm.matuszewski.programista@gmail.com>
2024-04-02 13:43:21 +0200

    First upload with .mm file

---
+++ 13_ConwoysGameOfLife/Conways Game Of Life.mm
@@ -0,0 +1,32 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1712051968282" ID="ID_358407568" MODIFIED="1712056848237" STYLE="bubble" TEXT="Conways Game Of Life">
+<node CREATED="1712052595152" ID="ID_128738357" MODIFIED="1712056848237" POSITION="right" STYLE="bubble" TEXT="Rules">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1712052615067" ID="ID_863217420" MODIFIED="1712056848237" STYLE="bubble" TEXT="Livin cells with 2-3 neighbours keep living"/>
+<node CREATED="1712052704872" ID="ID_576575812" MODIFIED="1712056848237" STYLE="bubble" TEXT="Dead cells with 3 neighbours comes to life"/>
+<node CREATED="1712052742872" ID="ID_1530082297" MODIFIED="1712056848237" STYLE="bubble" TEXT="Rest cells dies or keep being dead"/>
+</node>
+<node CREATED="1712052610858" ID="ID_1191547589" MODIFIED="1712056848237" POSITION="right" STYLE="bubble" TEXT="Manage &quot;Screen&quot;">
+<node CREATED="1712053233513" ID="ID_763811582" MODIFIED="1712056848237" STYLE="bubble" TEXT="First Draw of cells">
+<node CREATED="1712056689076" ID="ID_1794284232" MODIFIED="1712056848237" STYLE="bubble" TEXT="Based on rand"/>
+</node>
+<node CREATED="1712053240965" ID="ID_819177492" MODIFIED="1712057689117" STYLE="bubble" TEXT="Update sreen">
+<node CREATED="1712057757140" ID="ID_1474525115" MODIFIED="1712057763658" TEXT="Move through every cell">
+<node CREATED="1712057771279" ID="ID_1929552119" MODIFIED="1712057781542" TEXT="Count neighbours"/>
+<node CREATED="1712057782325" ID="ID_1212655600" MODIFIED="1712057816644" TEXT="Update following the rules">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1712056697852" ID="ID_1454094253" MODIFIED="1712056848238" STYLE="bubble" TEXT="Use method to draw"/>
+</node>
+</node>
+<node CREATED="1712052605417" ID="ID_726727635" MODIFIED="1712056848238" POSITION="right" STYLE="bubble" TEXT="Manage interface">
+<node CREATED="1712056772999" ID="ID_1059506302" MODIFIED="1712056886684" STYLE="bubble" TEXT="Proper display">
+<node CREATED="1712056894380" ID="ID_875323796" MODIFIED="1712056908669" TEXT="Delay"/>
+<node CREATED="1712056896753" ID="ID_418755059" MODIFIED="1712056905096" TEXT="Press button"/>
+</node>
+<node CREATED="1712056780746" ID="ID_1349880958" MODIFIED="1712056848238" STYLE="bubble" TEXT="End the program"/>
+</node>
+</node>
+</map>

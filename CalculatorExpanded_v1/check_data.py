eb978bde75db85981744c468957282330a18f137
JMMatuszewski <jmm.matuszewski.programista@gmail.com>
2025-01-05 19:58:38 +0100

    Refactor of basics

--- CalculatorExpanded_v1/check_data.py
+++ CalculatorExpanded_v1/check_data.py
@@ -1,10 +1,21 @@
 
 class CheckData:
 
-    def check_int(self, val):
-        if not type(val) == int:
-            return True
-        return False
+    # def check_int(self, val):
+    #     if not type(val) == int:
+    #         return True
+    #     return False
+
+    def ord_nums(self,val):
+        '''Ordinal numbers given depending on input value'''
+        if val == 1:
+            return 'st'
+        if val == 2:
+            return 'nd'
+        if val == 3:
+            return 'rd'
+        else:
+            return 'th'
 
     def check_div(self,val):
         if val == 0:
---
+++ CalculatorExpanded_v1/operations.py
@@ -0,0 +1,13 @@
+class Operations:
+
+    def add(self, val1, val2):
+        return val1 + val2
+
+    def sub(self, val1, val2):
+        return val1 - val2
+
+    def mul(self, val1, val2):
+        return val1 * val2
+
+    def div(self, val1, val2):
+        return val1 / val2

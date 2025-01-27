50c31f0e97a4882f1882b4ac665e87e8a0446e57
JMMatuszewski <jmm.matuszewski.programista@gmail.com>
2025-01-27 19:56:10 +0100

    pytest-setup-teardown

--- CalculatorExpanded_v1/tests/test_calculations.py
+++ CalculatorExpanded_v1/tests/test_calculations.py
@@ -1,74 +1,64 @@
+from webbrowser import Opera
+
 import pytest
 from calculations import Operations
 
-def test_add_positive():
+@pytest.fixture()
+def setup_operations():
     oper = Operations()
-    assert 5 == oper.add(2,3)
+    yield oper
 
-def test_add_positive_negative():
-    oper = Operations()
-    assert -1 == oper.add(2,-3)
+def test_add_positive(setup_operations):
+    assert 5 == setup_operations.add(2,3)
 
-def test_add_negative():
-    oper = Operations()
-    assert -5 == oper.add(-2,-3)
+def test_add_positive_negative(setup_operations):
+    assert -1 == setup_operations.add(2,-3)
 
-def test_add_fractions():
-    oper = Operations()
-    assert 1 == oper.add(1/2, 1/2)
+def test_add_negative(setup_operations):
+    assert -5 == setup_operations.add(-2,-3)
 
-def test_sub_positive():
-    oper = Operations()
-    assert 5 == oper.sub(10,5)
+def test_add_fractions(setup_operations):
+    assert 1 == setup_operations.add(1/2, 1/2)
 
-def test_sub_positive_negative():
-    oper = Operations()
-    assert 15 == oper.sub(10,-5)
+def test_sub_positive(setup_operations):
+    assert 5 == setup_operations.sub(10,5)
 
-def test_sub_negative():
-    oper = Operations()
-    assert -5 == oper.sub(-10,-5)
+def test_sub_positive_negative(setup_operations):
+    assert 15 == setup_operations.sub(10,-5)
 
-def test_sub_fractions():
-    oper = Operations()
-    assert -1/10 == oper.sub(1/10,1/5)
+def test_sub_negative(setup_operations):
+    assert -5 == setup_operations.sub(-10,-5)
 
-def test_mul_positive():
-    oper = Operations()
-    assert 25 == oper.mul(5,5)
+def test_sub_fractions(setup_operations):
+    assert -1/10 == setup_operations.sub(1/10,1/5)
 
-def test_mul_positive_negative():
-    oper = Operations()
-    assert -25 == oper.mul(5,-5)
+def test_mul_positive(setup_operations):
+    assert 25 == setup_operations.mul(5,5)
 
-def test_mul_negative():
-    oper = Operations()
-    assert 25 == oper.mul(-5,-5)
+def test_mul_positive_negative(setup_operations):
+    assert -25 == setup_operations.mul(5,-5)
+
+def test_mul_negative(setup_operations):
+    assert 25 == setup_operations.mul(-5,-5)
 
 @pytest.mark.xfail
-def test_mul_fractions():
-    oper = Operations()
-    assert 1/25 == oper.mul(1/5,1/5)
+def test_mul_fractions(setup_operations):
+    assert 1/25 == setup_operations.mul(1/5,1/5)
 
 
-def test_div_zero():
-    oper = Operations()
+def test_div_zero(setup_operations):
     with pytest.raises(ZeroDivisionError):
-        oper.div(5/0)
+        setup_operations.div(5,0)
 
-def test_div_positive():
-    oper = Operations()
-    assert 5 == oper.div(10,2)
+def test_div_positive(setup_operations):
+    assert 5 == setup_operations.div(10,2)
 
-def test_div_positive_negative():
-    oper = Operations()
-    assert -5 == oper.div(10,-2)
+def test_div_positive_negative(setup_operations):
+    assert -5 == setup_operations.div(10,-2)
 
-def test_div_negative():
-    oper = Operations()
-    assert 5 == oper.div(-10,-2)
+def test_div_negative(setup_operations):
+    assert 5 == setup_operations.div(-10,-2)
 
-def test_div_fractions():
-    oper = Operations()
-    assert 1/5 == oper.div(1/10,1/2)
+def test_div_fractions(setup_operations):
+    assert 1/5 == setup_operations.div(1/10,1/2)
 
